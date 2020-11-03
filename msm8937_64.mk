a94b32d1611b3bd91b7c5270e085ec89cace8700
zqm64 <zqm64@outlook.com>
2020-11-03 14:29:01 +0800

    santoni: Import fingerprint from old santoni dt
    Edit: We build libunwind by vndk/vndk-ext-libs.mk
    
    https://github.com/Stargazer19/device_xiaomi_santoni
    
    Co-authored-by: Stargazer19 <pviraj19@gmail.com>

--- msm8937_64.mk
+++ msm8937_64.mk
@@ -447,15 +447,22 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1 \
+    libunwind
+
 # Init
 PRODUCT_PACKAGES += \
+    init.goodix.sh \
     init.santoni.rc
 
 $(call inherit-product, $(LOCAL_PATH)/blobs/santoni/santoni-vendor.mk)
