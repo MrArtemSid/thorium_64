e5d6c0b5cb33fe5da1e31bee0a15807b58905a72
MrArtemSid <artem.sid.key@gmail.com>
2020-12-15 16:05:27 +0400

    msm8937_64: Add IR support

--- msm8937_64.mk
+++ msm8937_64.mk
@@ -403,6 +403,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml \
     $(LOCAL_PATH)/audio/mixer_paths_qrd_sku1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_qrd_sku1.xml
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
+# IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 $(call inherit-product, $(LOCAL_PATH)/blobs/santoni/santoni-vendor.mk)
 ###################################################################################
 # This is the End of target.mk file.
